66976f6c1172ff390f67171e0e07256f4ac8942c
huuuunee <s21024@gsm.hs.kr>
2025-04-08 12:29:31 +0900

    2025-04-08

---
+++ programing/first-week/homework.c
@@ -0,0 +1,21 @@
+#pragma warning (disable:4956)
+
+#include<stdio.h>
+
+int main() {
+    int inputData, second, minute, hour;
+
+    printf("초를 입력해주세요 : ");
+    scanf("%d", &inputData);
+
+    hour = inputData / 3600;
+    inputData = inputData % 3600;
+
+    minute = inputData / 60;
+    inputData = inputData % 60;
+    
+    second = inputData;
+
+    printf("%d시 %d분 %d초 입니다.", hour, minute, second);
+    return 0;
+}
